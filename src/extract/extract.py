58bd1f2d2ccd2bef44ad24f446281a78a38c58c7
gabrielprozin <gabrielalexandrino2004@gmail.com>
2026-04-27 23:09:50 -0300

    Primeiros passos no extract e criação do .csv de vendas

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,20 +1,40 @@
 from sqlalchemy import text
-
+from pathlib import Path
 from db.conexao_db import get_connection
+import csv
 
+QUERY_PATH = Path(__file__).resolve().parent / "queries" / "vendas.sql"
+
+def read_query():
+    return QUERY_PATH.read_text(encoding="utf-8")
 
 def extract_data():
-    query = text(
-        """
-        SELECT FIRST 10 *
-        FROM TBDUPLICATA
-        """
-    )
+    query = read_query()
 
     with get_connection() as conn:
-        rows = conn.execute(query).fetchall()
+        result = conn.execute(text(query))
 
-    for row in rows:
-        print(row)
+        rows = result.fetchall()
+        columns = result.keys()
+    
+    
+    print(columns)
+    print(rows[:5])
+
+    output_path = Path("data/raw/vendas.csv")
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with output_path.open("w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(columns)
+        writer.writerows(rows)
 
     return len(rows)
+
+
+
+
+
+
+if __name__ == "__main__":
+    extract_data()
