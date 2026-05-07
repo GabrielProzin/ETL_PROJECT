8febc04b9798fdf7edf46e270bdfc4c4894845fd
gabrielprozin <gabrielalexandrino2004@gmail.com>
2026-05-06 23:18:57 -0300

    Iniciando função de transform_data

--- src/transform/transform_alunos_ativos.py
+++ src/transform/transform_alunos_ativos.py
@@ -3,3 +3,8 @@ from pathlib import Path
 
 def transform_data():
     input_path = Path("data/raw/alunos.csv")
+    output_path = Path("data/processed/alunos.csv")
+
+    df = pd.read_csv(input_path)
+
+    
