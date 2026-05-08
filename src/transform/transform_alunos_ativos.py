59142a36bded6d8650751bcc8b7a9eeddcb1ff73
gabrielprozin <gabrielalexandrino2004@gmail.com>
2026-05-07 22:22:48 -0300

    Finalizando transform de alunos

--- src/transform/transform_alunos_ativos.py
+++ src/transform/transform_alunos_ativos.py
@@ -7,4 +7,18 @@ def transform_data():
 
     df = pd.read_csv(input_path)
 
-    
+    df["nome_aluno"] = df["nome_aluno"].str.strip()
+    df["genero"] = df["genero"].str.strip()
+    df["serie"] = df["serie"].str.strip()
+    df["turma"] = df["turma"].str.strip()
+
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+
+    df.to_csv(output_path, index=False, encoding="utf-8")
+
+    print(f"{len(df)} linhas transformadas.")
+
+    return len(df)
+
+if __name__ == "__main__":
+    transform_data()
